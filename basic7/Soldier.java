c54689e0b994069966847f1dcc7960382a647deb
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2025-01-13 23:19:30 +0100

    unify SoliderTower, SoldierResource.

--- basic7/Soldier.java
+++ basic7/Soldier.java
@@ -6,21 +6,43 @@ public class Soldier extends Unit {
 
     boolean recovering = false;
 
+    static enum Mode {
+        TOWERS,
+        RESOURCES,
+    }
+
+    Mode mode;
+
     Soldier(RobotController rc) throws GameActionException {
         super(rc);
         Map.initialize();
+
+        mode = Mode.TOWERS;
+        if(rc.getRoundNum() > 100 && Math.random() < 0.3) {
+            mode = Mode.RESOURCES;
+        }
     }
 
     void startTurn() throws GameActionException {
         Map.fill();
+        updateClosestRuin();
     }
 
     void endTurn(){
     }
 
     void runTurn() throws GameActionException {
-
+        attackTowers();
+        completePatterns();
+        tryWithdraw();
+        if (shouldRecover()) recovering = true;
+        if (rc.getPaint() >= UnitType.SOLDIER.paintCapacity - Constants.MIN_TRANSFER_PAINT) recovering = false;
+        move();
+        paint();
+        tryWithdraw();
+        completePatterns();
     }
+
 
     void paintSelf() throws GameActionException {
         if (!rc.canAttack(rc.getLocation())) return;
@@ -67,6 +89,99 @@ public class Soldier extends Unit {
             if (!r.getType().isTowerType()) continue;
             if (rc.canAttack(r.getLocation())) rc.attack(r.getLocation());
         }
+    }
+
+    boolean shouldRecover(){
+        return ((closestRuin == null || rc.getLocation().distanceSquaredTo(closestRuin) > 2 ||  Util.towerMax()) && rc.getPaint() < Constants.CRITICAL_PAINT_SOLDIER_TOWER);
+    }
+
+    void move() throws GameActionException {
+        if (!rc.isMovementReady()) return;
+        if (MicroManagerSoldier.doMicro()) return;
+        MapLocation target = getTarget();
+        pathfinding.moveTo(target);
+    }
+
+    MapLocation getTarget() throws GameActionException{
+        return switch(mode) {
+            case TOWERS -> getTargetTowers();
+            case RESOURCES -> getTargetResources();
+            default -> getTargetTowers();
+        };
+    }
+
+    MapLocation getTargetTowers() throws GameActionException{
+        if (recovering && TowerManager.closestPaintTower != null) return TowerManager.closestPaintTower;
+        if (closestRuin != null && !Util.towerMax()) return closestRuin;
+        else if (rc.getNumberTowers() < GameConstants.MAX_NUMBER_OF_TOWERS) return explore.getExplore3Target();
+        MapLocation loc = getClosestEmptyTile();
+        if (loc != null) return loc;
+        return explore.getExplore3Target();
+    }
+
+
+
+    MapLocation getTargetResources() throws GameActionException{
+        if (recovering && TowerManager.closestPaintTower != null) return TowerManager.closestPaintTower;
+        if (rc.getRoundNum() > creationTurn) {
+            MapLocation target = ResourcePatternManager.getBestTarget();
+            //MapLocation target = null;
+            if (target != null){
+                rc.setIndicatorDot(target, 100, 100, 100);
+                return target;
+            }
+        }
+        return explore.getExplore3Target();
+    }
+
+
+    void paint() throws GameActionException {
+        attackTowers();
+        if (closestRuin == null || rc.getLocation().distanceSquaredTo(closestRuin) > 8 || Util.towerMax()){
+            paintSelf();
+            return;
+        }
+        int ruinType = Map.getPattern(closestRuin);
+        //System.out.println("ruinType: " + ruinType);
+        int trueType = RuinManager.getType(ruinType);
+        switch(trueType){
+            case -1 -> chooseWisely(ruinType);
+            default -> {
+                UnitType t = switch(trueType){
+                    case RuinManager.PAINT -> UnitType.LEVEL_ONE_PAINT_TOWER;
+                    case RuinManager.MONEY -> UnitType.LEVEL_ONE_MONEY_TOWER;
+                    default -> UnitType.LEVEL_ONE_DEFENSE_TOWER;
+                };
+                //System.out.println("Trying to complete tower of type " + t);
+                if (rc.canCompleteTowerPattern(t, closestRuin)){
+                    rc.completeTowerPattern(t, closestRuin);
+                    return;
+                }
+                RuinManager.drawPatternEnhanced(closestRuin,trueType);
+            }
+        }
+    }
+
+    void chooseWisely(int ruinType) throws GameActionException{
+        if ((ruinType & 8) > 0) paintRuinSafe();
+        try {
+            RuinManager.drawPatternEnhanced(closestRuin, TowerManager.getNextBuild());
+        } catch(GameActionException e){
+            e.printStackTrace();
+            //System.out.println("My Location " + rc.getLocation());
+            //System.out.println("Ruin Location " + closestRuin);
+        }
+    }
+
+    void paintRuinSafe() throws GameActionException {
+        MapLocation loc = closestRuin.add(Direction.NORTHEAST);
+        if (rc.canSenseLocation(loc) && rc.senseMapInfo(loc).getPaint() == PaintType.EMPTY && rc.canAttack(loc)) rc.attack(loc, true);
+        loc = closestRuin.add(Direction.SOUTHEAST);
+        if (rc.canSenseLocation(loc) && rc.senseMapInfo(loc).getPaint() == PaintType.EMPTY && rc.canAttack(loc)) rc.attack(loc, true);
+        loc = closestRuin.add(Direction.SOUTHWEST);
+        if (rc.canSenseLocation(loc) && rc.senseMapInfo(loc).getPaint() == PaintType.EMPTY && rc.canAttack(loc)) rc.attack(loc, true);
+        loc = closestRuin.add(Direction.NORTHWEST);
+        if (rc.canSenseLocation(loc) && rc.senseMapInfo(loc).getPaint() == PaintType.EMPTY && rc.canAttack(loc)) rc.attack(loc, true);
     }
 
 }
