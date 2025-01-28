507e172bc11c555d5a0bcd4cbcf157bc9f62bcf3
IvanGeffner <ivanegeffner@gmail.com>
2025-01-28 08:13:14 +0100

    rush

---
+++ basic41/Pathfinding.java
@@ -0,0 +1,65 @@
+package basic41;
+
+import battlecode.common.*;
+
+public class Pathfinding {
+
+        //static RobotController rc;
+        static BugNav bugNav;
+        static final int GREEDY_TURNS = 4;
+        static int turnsGreedy = 0;
+        static MapLocation currentTarget = null;
+
+        static void reset(){
+            turnsGreedy = 0;
+            MapTracker.reset();
+            bugNav.resetPathfinding();
+        }
+
+    void update(MapLocation target){
+        if (currentTarget == null || target.distanceSquaredTo(currentTarget) > 0){
+            reset();
+        } else --turnsGreedy;
+        currentTarget = target;
+        MapTracker.add(MyRobot.rc.getLocation());
+    }
+
+        Pathfinding(){
+            //Pathfinding.rc = MyRobot.rc;
+            bugNav = new BugNav();
+        }
+
+        void moveTo(MapLocation target) throws GameActionException {
+            //if (target != null) MyRobot.rc.setIndicatorString("Going to " + target);
+            //if (bugNav.isUsingBugnav()) bugNav.moveTo(target);
+            //Direction dir = BFS.getBestDir(target);
+            RobotController rc = MyRobot.rc;
+
+            if (target == null) return;
+
+            if (rc.onTheMap(target)) rc.setIndicatorDot(target, 0, 0, 200);
+            //rc.setIndicatorString("Going to " + target);
+
+            if (!rc.isMovementReady()) return;
+            if (rc.getLocation().distanceSquaredTo(target) == 0) return;
+
+            update(target);
+
+            if (!bugNav.isUsingBugnav() && turnsGreedy <= 0){
+                //VisionManager.computeDistsUnit();
+
+                //System.err.println("Using bfs");
+                Direction dir = VisionManager.getBestDirection(target);
+                if (dir != null && !MapTracker.check(rc.getLocation().add(dir))){
+                    MovementManager.move(dir);
+                    //return;
+                } else {
+                    turnsGreedy = GREEDY_TURNS;
+                    bugNav.moveTo(target);
+                }
+            }
+            else {
+                bugNav.moveTo(target);
+            }
+        }
+}
---
+++ basic41/SymmetryManager.java
@@ -0,0 +1,104 @@
+package basic41;
+
+import battlecode.common.*;
+
+public class SymmetryManager {
+
+    static final int H = 1, V = 2, R = 4;
+    static final int HC = (~H)&7, VC = (~V)&7, RC = (~R)&7;
+
+    static boolean foundH = false, foundR = false, foundV = false;
+
+    static MapLocation base = null;
+    static MapLocation bR, bH, bV;
+
+    static MapLocation target;
+
+    static int discardedSyms = 0;
+
+    static MapLocation L;
+    static int symX, symY;
+
+    static long[] mapWalls = new long[60];
+    static long[] mapRuins = new long[60];
+    static long[] mapVision = new long[60];
+
+    static void discardH(){
+        discardedSyms |= H;
+    }
+
+    static void discardV(){
+        discardedSyms |= V;
+    }
+
+    static void discardR(){
+        discardedSyms |= R;
+    }
+
+    static int getSym(){
+        return switch(discardedSyms){
+            case HC -> H;
+            case VC -> V;
+            case RC -> R;
+            default -> 0;
+        };
+    }
+
+    static void checkSym(){
+        if (base == null) return;
+        if (Clock.getBytecodesLeft() < 400) return;
+        if (getSym() != 0) return;
+        MapInfo[] infos = MyRobot.rc.senseNearbyMapInfos();
+        for (MapInfo m : infos){
+            if (Clock.getBytecodesLeft() < 400) return;
+            L = m.getMapLocation();
+            mapVision[L.x] |= (1L << L.y);
+            if (m.isWall()) mapWalls[L.x] |= (1L << L.y);
+            else if (m.hasRuin()) mapRuins[L.x] |= (1L << L.y);
+            symX = MyRobot.W - L.x - 1;
+            symY = MyRobot.H - L.y - 1;
+            if ((mapVision[symX] & (1L << L.y)) != 0 && (mapWalls[L.x] & (1L << L.y)) != (mapWalls[symX] & (1L << L.y))) discardH();
+            if ((mapVision[L.x] & (1L << symY)) != 0 && (mapWalls[L.x] & (1L << L.y)) != (mapWalls[L.x] & (1L << symY))) discardV();
+            if ((mapVision[symX] & (1L << symY)) != 0 && (mapWalls[L.x] & (1L << L.y)) != (mapWalls[symX] & (1L << symY))) discardR();
+            if ((mapVision[symX] & (1L << L.y)) != 0 && (mapRuins[L.x] & (1L << L.y)) != (mapRuins[symX] & (1L << L.y))) discardH();
+            if ((mapVision[L.x] & (1L << symY)) != 0 && (mapRuins[L.x] & (1L << L.y)) != (mapRuins[L.x] & (1L << symY))) discardV();
+            if ((mapVision[symX] & (1L << symY)) != 0 && (mapRuins[L.x] & (1L << L.y)) != (mapRuins[symX] & (1L << symY))) discardR();
+        }
+    }
+
+    static MapLocation getSymmetric(MapLocation loc){
+        return switch(getSym()){
+            case H-> new MapLocation(MyRobot.W - loc.x - 1, loc.y);
+            case V-> new MapLocation(loc.x, MyRobot.H - loc.y - 1);
+            case R-> new MapLocation(MyRobot.W - loc.x - 1, MyRobot.H - loc.y - 1);
+            default -> null;
+        };
+    }
+
+    static void setBase(MapLocation loc){
+        base = loc;
+        bR =  new MapLocation(MyRobot.W - base.x - 1, MyRobot.H - base.y - 1);
+        bV = new MapLocation(base.x, MyRobot.H - base.y - 1);
+        bH = new MapLocation(MyRobot.W - base.x - 1, base.y);
+    }
+
+    static MapLocation getTarget(){
+        if (base == null) return null;
+        target = null;
+        MapLocation myLoc = MyRobot.rc.getLocation();;
+        if ((discardedSyms & R) == 0 && !foundR){
+            if (myLoc.distanceSquaredTo(bR) < 10) foundR = true;
+            else return bR;
+        }
+        if ((discardedSyms & V) == 0 && !foundV){
+            if (myLoc.distanceSquaredTo(bV) < 10) foundV = true;
+            else return bV;
+        }
+        if ((discardedSyms & H) == 0 && !foundH){
+            if (myLoc.distanceSquaredTo(bH) < 10) foundH = true;
+            else return bH;
+        }
+        return null;
+    }
+
+}
